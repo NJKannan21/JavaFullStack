f64d9b63f8ba30f9a2c1948be44cf3822ef2e6c4
NJKannan21 <kannan21.nj@gmail.com>
2024-12-27 16:19:52 +0530

    Effective fix

--- src/guviPortalTaskOne/printPattern.java
+++ src/guviPortalTaskOne/printPattern.java
@@ -12,23 +12,36 @@ public class printPattern {
 		// 5 4 3 2 2
 		// 5 4 3 2 1
 		
-		int controller = 0;
-		int occurrence = 0;
+//		int controller = 0;
+//		int occurrence = 0;
 		
-		for(int i=0; i<5; i++) {
-			for(int j=0; j<5; j++) {
-				for(int k=5; k>0; k--) {
-					if( ( i == controller || j == controller ) && occurrence == 0 ) {
-						System.out.print(k);
-						occurrence++;
-					}
-						controller++;
-				}
-				controller = 0;
-				occurrence = 0;
-				System.out.print("\t");
+//		for(int i=0; i<5; i++) {
+//			for(int j=0; j<5; j++) {
+//				for(int k=5; k>0; k--) {
+//					if( ( i == controller || j == controller ) && occurrence == 0 ) {
+//						System.out.print(k);
+//						occurrence++;
+//					}
+//						controller++;
+//				}
+//				controller = 0;
+//				occurrence = 0;
+//				System.out.print("\t");
+//			}
+//			System.out.println("\n");
+//		}
+		
+		
+		int n = 5;
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<i; j++) {
+				System.out.print(n - j + 1 + " ");
 			}
-			System.out.println("\n");
+			for(int k=i; k<=n; k++) {
+				System.out.print(n - i + 1 + " ");
+			}
+			System.out.println();
 		}
 	}
 }
